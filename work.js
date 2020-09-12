f1736708e0f682a2148c461d612a12d0511fe164
Keith Bailey <bailey.keith.allen@gmail.com>
2020-09-12 12:32:08 -0400

    adding button for anterior

--- work.js
+++ work.js
@@ -1,5 +1,8 @@
-const work = document.getElementById('push')
-work.addEventListener('click', () => { document.getElementById('work').innerHTML += " " + push[1] })
+const psh = document.getElementById('push')
+psh.addEventListener('click', () => { document.getElementById('work').innerHTML += " " + push[1] })
 
 const pll = document.getElementById('pull')
 pll.addEventListener('click', () => { document.getElementById('work').innerHTML += " " + pull[1] })
+
+const ant = document.getElementById('anterior')
+ant.addEventListener('click', () => { document.getElementById('work').innerHTML += " " + anterior[1] })
