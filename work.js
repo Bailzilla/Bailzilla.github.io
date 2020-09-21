a6319b1d79bf94963186e8567591f87a1bf72d49
Keith Bailey <bailey.keith.allen@gmail.com>
2020-09-21 10:52:56 -0400

    working on quote display

--- work.js
+++ work.js
@@ -10,10 +10,15 @@ ant.addEventListener('click', () => { document.getElementById('work').innerHTML 
 const pos = document.getElementById('posterior')
 pos.addEventListener('click', () => { document.getElementById('work').innerHTML += " " + posterior[Math.floor(Math.random() * posterior.length)] })
 
+
+let arr = []
+
 fetch("https://type.fit/api/quotes")
   .then(function(response) {
     return response.json();
   })
   .then(function(data) {
-    console.log(data);
+    document.getElementById('quote').textContent=data[1];
   });
+
+  
