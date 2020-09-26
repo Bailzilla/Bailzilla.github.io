2e498cf240bd78dd5f16173b671a007c19a3b7c6
Keith Bailey <bailey.keith.allen@gmail.com>
2020-09-26 11:02:34 -0400

    reformatting arrays + display syntax

--- work.js
+++ work.js
@@ -1,14 +1,14 @@
 const psh = document.getElementById('push')
-psh.addEventListener('click', () => { document.getElementById('work').innerHTML += " " + push[Math.floor(Math.random() * push.length)] })
+psh.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + push[Math.floor(Math.random() * push.length)] })
 
 const pll = document.getElementById('pull')
-pll.addEventListener('click', () => { document.getElementById('work').innerHTML += " " + pull[Math.floor(Math.random() * pull.length)] })
+pll.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + pull[Math.floor(Math.random() * pull.length)] })
 
 const ant = document.getElementById('anterior')
-ant.addEventListener('click', () => { document.getElementById('work').innerHTML += " " + anterior[Math.floor(Math.random() * anterior.length)] })
+ant.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + anterior[Math.floor(Math.random() * anterior.length)] })
 
 const pos = document.getElementById('posterior')
-pos.addEventListener('click', () => { document.getElementById('work').innerHTML += " " + posterior[Math.floor(Math.random() * posterior.length)] })
+pos.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + posterior[Math.floor(Math.random() * posterior.length)] })
 
 
 let arr = []
