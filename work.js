500d4438b0f791eae4b3c65ded12e2f7a9d16180
Keith Bailey <bailey.keith.allen@gmail.com>
2020-10-11 12:37:19 -0400

    changing button hover color

--- work.js
+++ work.js
@@ -1,14 +1,16 @@
+//add a function to 'add an exercise' to replace repeated calls
+
 const psh = document.getElementById('push')
-psh.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + push[Math.floor(Math.random() * push.length)] })
+  psh.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + push[Math.floor(Math.random() * push.length)] })
 
-const pll = document.getElementById('pull')
-pll.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + pull[Math.floor(Math.random() * pull.length)] })
+  const pll = document.getElementById('pull')
+  pll.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + pull[Math.floor(Math.random() * pull.length)] })
 
-const ant = document.getElementById('anterior')
-ant.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + anterior[Math.floor(Math.random() * anterior.length)] })
+  const ant = document.getElementById('anterior')
+  ant.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + anterior[Math.floor(Math.random() * anterior.length)] })
 
-const pos = document.getElementById('posterior')
-pos.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + posterior[Math.floor(Math.random() * posterior.length)] })
+  const pos = document.getElementById('posterior')
+  pos.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + posterior[Math.floor(Math.random() * posterior.length)] })
 
 fetch("https://type.fit/api/quotes")
   .then(response => response.json())
