be6c25d52222157aedba368efe1c80e5d1946cfd
Keith Bailey <bailey.keith.allen@gmail.com>
2020-10-12 12:05:34 -0400

    adding random number variable

--- work.js
+++ work.js
@@ -1,4 +1,13 @@
 //add a function to 'add an exercise' to replace repeated calls
+// declare varialbles: elements and random number
+
+let rando = Math.floor(Math.random() * push.length)
+console.log(rando)
+
+//document.getElementsByClassName('button')
+
+exer=>exer.addEventListener('click', document.getElementById('work').innerHTML += exer[rando])
+
 
 const psh = document.getElementById('push')
   psh.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + push[Math.floor(Math.random() * push.length)] })
@@ -12,9 +21,12 @@ const psh = document.getElementById('push')
   const pos = document.getElementById('posterior')
   pos.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + posterior[Math.floor(Math.random() * posterior.length)] })
 
-fetch("https://type.fit/api/quotes")
+
+//API call for random quote
+
+  fetch("https://type.fit/api/quotes")
   .then(response => response.json())
     .then(data => document.getElementById('quote').innerHTML=(data[Math.floor(Math.random() * data.length)].text))
 
 
-exer=>exer.addEventListener('click', document.getElementById('work').innerHTML += exer[Math.floor(Math.random()*exer.length)])
+
