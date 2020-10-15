9ddbe6682bd88856ed05d98999d78fc2868d85bb
Keith Bailey <bailey.keith.allen@gmail.com>
2020-10-15 08:00:31 -0400

    implementing random number variable

--- work.js
+++ work.js
@@ -9,20 +9,21 @@ console.log(rando)
 exer=>exer.addEventListener('click', document.getElementById('work').innerHTML += exer[rando])
 
 
-const psh = document.getElementById('push')
-  psh.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + push[Math.floor(Math.random() * push.length)] })
+          const psh = document.getElementById('push')
+          psh.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + push[rando] })
 
-  const pll = document.getElementById('pull')
-  pll.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + pull[Math.floor(Math.random() * pull.length)] })
+          const pll = document.getElementById('pull')
+          pll.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + pull[rando] })
 
-  const ant = document.getElementById('anterior')
-  ant.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + anterior[Math.floor(Math.random() * anterior.length)] })
+          const ant = document.getElementById('anterior')
+          ant.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + anterior[rando] })
 
-  const pos = document.getElementById('posterior')
-  pos.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + posterior[Math.floor(Math.random() * posterior.length)] })
+          const pos = document.getElementById('posterior')
+          pos.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + posterior[rando] })
 
 
-//API call for random quote
+  
+          //API call for random quote
 
   fetch("https://type.fit/api/quotes")
   .then(response => response.json())
