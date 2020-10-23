836270ed13f5d1cae4e6f9a5717799be2f8df247
Keith Bailey <bailey.keith.allen@gmail.com>
2020-10-22 21:05:36 -0400

    white to green

--- work.js
+++ work.js
@@ -1,10 +1,10 @@
 //add a function to 'add an exercise' to replace repeated calls
 // declare varialbles: elements and random number
 
-// let rando = Math.floor(Math.random() * arr.length)
-// console.log(rando)
+  // let rando = Math.floor(Math.random() * arr.length)
+  // console.log(rando)
 
-//document.getElementsByClassName('button')
+    //document.getElementsByClassName('button')
 
 exer=>exer.addEventListener('click', document.getElementById('work').innerHTML += exer[rando])
 
@@ -22,14 +22,5 @@ exer=>exer.addEventListener('click', document.getElementById('work').innerHTML +
 
           const pos = document.getElementById('posterior')
           pos.addEventListener('click', () => { document.getElementById('work').innerHTML += " ~" + posterior[rando] })
-
-
-  
-          //API call for random quote
-
-  // fetch("https://type.fit/api/quotes")
-  // .then(response => response.json())
-  //   .then(data => document.getElementById('quote').innerHTML=(data[Math.floor(Math.random() * data.length)].text))
-
 
 
