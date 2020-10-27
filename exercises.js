9fd683338f7edd38d98290a7bb3f29538a5b2937
Keith Bailey <bailey.keith.allen@gmail.com>
2020-10-27 12:30:58 -0400

    updating button functionality

--- exercises.js
+++ exercises.js
@@ -38,4 +38,3 @@ const posterior = [
     'swing'
 ]
 
-        
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,21 @@
 import logo from './gym.jpg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
-        <h1 id='quote'></h1>
+        <h1 id='quote'>yo</h1>
         <img src={logo} className="App-logo" alt="logo" />
         
  
-        <div class = 'container'>
-          <button class='button' id ="push">Add push exercise</button>
-          <button class='button' id="pull">Add pull exercise</button>
-          <button class='button' id="anterior">Add anterior lower body exercise</button>
-          <button class='button' id="posterior">Add posterior lower body exercise</button>
+        <div className = 'container'>
+          <button className='button' onClick={alert('hi')} >push</button>
+          <button className='button' >Add pull exercise</button>
+          <button className='button' >Add anterior lower body exercise</button>
+          <button className='button' >Add posterior lower body exercise</button>
         </div>
 
         <p id="work">For today: </p>
